cfd4bcec69dd437858ddef3e7bdd51a66189cc06
Godfrey Ndiritu <godfreyndiritu26@gmail.com>
2023-08-23 19:27:25 +0300

    minor change

---
+++ controllers/user.js
@@ -0,0 +1,47 @@
+import jwt from "jsonwebtoken"
+import bcrypt from "bcryptjs"
+import User from "../models/User.js"
+
+// const generateToken = (id, username) => {
+//     return jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: "1d"})
+// }
+// register user
+export const register = async (req, res) => {
+	const { username, password, email } = req.body
+	// const userInfo = await User.create( { username, password, email } )
+	console.log(req.body)
+	try {
+		if (!email || !username || !password) {
+	        res.status(400)
+	        throw new Error("Please enter all fiedls")
+	    } 
+
+	    if (password.length < 6) {
+	        res.status(400)
+	        throw new Error("Password must be more than 6 characters");
+	    } 
+
+	    const userExists = await User.findOne({ email })
+	    if (userExists) {
+	        res.status(400)
+	        throw new Error("There exists a user registered with that email already")
+	    } 
+
+	    const registeredUser = await User.create({
+	        username,
+	        password,
+	        email
+	    });
+	    // generateToken(registeredUser._id, username)
+	    jwt.sign({ userId: registeredUser._id }, process.env.JWT_SECRET, {}, (err, token) => {
+	    	if (err) throw new Error("No token found") 
+    		return res.cookie("token", token).status(201).json({
+    			_id: registeredUser._id
+    		})
+	    })
+	    return res.status(200).json({ data: registeredUser })
+	} catch (error) {
+		if (error) throw new Error("Something went wrong")
+	}
+	// return res.status(200).json({ data: registeredUser })
+}
--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 import express from "express"
+import dotenv from 'dotenv'
+import mongoose from "mongoose"
+import cors from "cors"
+import userRoutes from "./routes/user.js"
 
+dotenv.config()
 const app = express()
+app.use(express.json())
+app.use(cors({
+	origin: process.env.CLIENT_URL,
+	credentials: true
+}))
 
-app.get('/', (req, res) => {
-	res.json('test ok')
+// connection to the mongodb
+const MONGO_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT || 8080
+
+mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true}).then(() => app.listen(PORT, () => console.log(`Success! Database running on port: ${PORT}`))).catch(error => console.log(error))
+
+mongoose.connection.on("disconnected", () => {
+    console.log("MongoDatabase disconnected")
 })
 
-app.listen(5000)
+
+app.use('/', userRoutes) 
+
+// app.listen(process.env.PORT)
+
